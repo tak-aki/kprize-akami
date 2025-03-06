e8547b593c08cc8fdb0c4bd56f75a66852c0deb4
unonao <naokiuno634@gmail.com>
2025-03-06 08:48:40 +0000

    コード修正

--- local/train/exp001/inference.py
+++ local/train/exp001/inference.py
@@ -1,10 +1,10 @@
-import json
 import logging
 import math
 import os
 import sys
 import textwrap
 from dataclasses import dataclass, field
+from glob import glob
 from pathlib import Path
 from typing import List
 
@@ -13,14 +13,12 @@ import numpy as np
 import pandas as pd
 import torch
 import vllm
-from datasets import Dataset
 from hydra.core.config_store import ConfigStore
 from hydra.core.hydra_config import HydraConfig
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import StratifiedKFold
 from tqdm import tqdm
 from transformers import AutoTokenizer, LogitsProcessor
-from vllm import LLMEngine, SamplingParams
 from vllm.lora.request import LoRARequest
 
 from local.src.utils import set_seed
@@ -28,7 +26,15 @@ from local.src.utils import set_seed
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from .run import setup_swe_verified_data
 
-CANDIDATE = ["1", "2", "3"]
+# 3クラス分類
+DIFFICULTY2TOKEN = {
+    "<15 min fix": "easy",
+    "15 min - 1 hour": "midium",
+    "1-4 hours": "difficult",
+    ">4 hours": "difficult",
+}
+TOKENS = ["easy", "midium", "difficult"]
+TOKEN2LABEL = {token: i for i, token in enumerate(TOKENS)}
 
 # ログ設定
 logger = logging.getLogger(__name__)
@@ -51,28 +57,28 @@ class ExpConfig:
     seed: int = 1029
     # 推論時に評価するfold（Stratified K-Foldの設定に合わせる）
     folds: List[int] = field(default_factory=lambda: [0])
+    n_epochs: int = 0
 
     model_name: str = "inarikami/DeepSeek-R1-Distill-Qwen-32B-AWQ"
-    lora_checkpoint: str = "output_train/exp000/001/fold0/checkpoint-250/adapter_model"
-    max_length: int = 3072
+    max_length: int = 8192
     lora_r: int = 16
     prompt_template: str = textwrap.dedent("""
         Below is a GitHub-related issue.
 
         {problem_statement}
 
-        Please review the problem statement and estimate how long it will take to resolve the issue. Choose the appropriate number from the options below:
-            1.	<15 min fix
-            2.	15 min - 1 hour
-            3.	>1 hours
+        Please review the problem statement and estimate how long it will take to resolve the issue. Choose the appropriate word from the options below:
+            easy: <15 min fix
+            medium: 15 min - 1 hour
+            difficult: >1 hour
 
-        Answer only the number (1, 2, or 3).
+        Answer only with one word (easy, medium, or difficult).
         """)
 
 
 @dataclass
 class EnvConfig:
-    exp_output_dir: str = "output_infer"
+    exp_output_dir: str = "output_train"
 
 
 @dataclass
@@ -115,35 +121,21 @@ def make_inference_prompt(cfg: Config, row, tokenizer):
 
 
 def generate_text_vllm(cfg, prompt_text_list, tokenizer, lora_path):
-    KEEP = []
-    for x in CANDIDATE:
+    keep_ids = []
+    for x in TOKENS:
         c = tokenizer.encode(x, add_special_tokens=False)[0]
-        KEEP.append(c)
-    print(f"Force predictions to be tokens {KEEP} which are {CANDIDATE}.")
+        keep_ids.append(c)
+    print(f"Force predictions to be tokens {keep_ids} which are {TOKENS}.")
 
     class DigitLogitsProcessor(LogitsProcessor):
         def __init__(self, tokenizer):
-            self.allowed_ids = KEEP
+            self.allowed_ids = keep_ids
 
         def __call__(self, input_ids: List[int], scores: torch.Tensor) -> torch.Tensor:
             scores[self.allowed_ids] += 100
             return scores
 
     logits_processors = [DigitLogitsProcessor(tokenizer)]
-    llm = vllm.LLM(
-        cfg.exp.model_name,
-        quantization="awq" if "awq" in cfg.exp.model_name.lower() else None,
-        tensor_parallel_size=torch.cuda.device_count(),
-        gpu_memory_utilization=0.80,
-        trust_remote_code=True,
-        dtype="half",
-        enforce_eager=True,
-        max_model_len=4096,  # if "7b" in cfg.exp.model_name.lower() else (5840 if IS_KAGGLE else 8096),
-        disable_log_stats=True,
-        # enable_prefix_caching=True,
-        enable_lora=True,
-        max_lora_rank=cfg.exp.lora_r,
-    )
     sampling_params = vllm.SamplingParams(
         n=1,  # Number of output sequences to return for each prompt.
         top_p=0.9,  # Float that controls the cumulative probability of the top tokens to consider.
@@ -154,11 +146,27 @@ def generate_text_vllm(cfg, prompt_text_list, tokenizer, lora_path):
         logits_processors=logits_processors,
         logprobs=5,
     )
+
+    llm = vllm.LLM(
+        cfg.exp.model_name,
+        quantization="awq" if "awq" in cfg.exp.model_name.lower() else None,
+        tensor_parallel_size=torch.cuda.device_count(),
+        gpu_memory_utilization=0.80,
+        trust_remote_code=True,
+        dtype="half",
+        enforce_eager=True,
+        max_model_len=cfg.exp.max_length,
+        disable_log_stats=True,
+        # enable_prefix_caching=True,
+        enable_lora=True,
+        max_lora_rank=cfg.exp.lora_r,
+    )
+
     responses = llm.generate(
         prompt_text_list,
         sampling_params=sampling_params,
         use_tqdm=True,
-        lora_request=LoRARequest("sql_adapter", 1, lora_path),
+        lora_request=LoRARequest("sql_adapter", 1, lora_path=lora_path),
     )
 
     results = []
@@ -168,7 +176,7 @@ def generate_text_vllm(cfg, prompt_text_list, tokenizer, lora_path):
         try:
             x = response.outputs[0].logprobs[0]
             logprobs = []
-            for k in KEEP:
+            for k in keep_ids:
                 if k in x:
                     logprobs.append(math.exp(x[k].logprob))
                 else:
@@ -179,7 +187,7 @@ def generate_text_vllm(cfg, prompt_text_list, tokenizer, lora_path):
             results.append(logprobs)
         except:
             # print(f"error {i}")
-            results.append(np.array([0.25, 0.25, 0.25]))
+            results.append(np.array([0.25] * len(keep_ids)))
             errors += 1
     pred_array = np.array(results).reshape((-1, 3))
 
@@ -204,38 +212,52 @@ def main(cfg: Config) -> None:
     ):
         df.loc[val_idx, "fold"] = fold_idx
 
-    # 推論処理
-
-    # 学習時と同様のdifficulty->ラベルのマッピング（0,1,2）
-    df["y_label"] = df["difficulty"].map({"<15 min fix": 1, "15 min - 1 hour": 2, "1-4 hours": 3, ">4 hours": 3})
+    df["y_word"] = df["difficulty"].map(DIFFICULTY2TOKEN)
+    df["y_label"] = df["y_word"].map(TOKEN2LABEL)
     tokenizer = setup_tokenizer(cfg.exp.model_name)
     df["prompt"] = df.apply(lambda row: make_inference_prompt(cfg, row, tokenizer), axis=1)
 
     for fold in cfg.exp.folds:
-        val_df = df[df["fold"] == fold].reset_index(drop=True)
-        logger.info(f"Number of validation samples: {len(val_df)}")
+        checkpoint_paths = sorted(
+            glob(f"{cfg.env.exp_output_dir}/{exp_name}/fold{fold}/checkpoint-*"),
+            key=lambda x: int(Path(x).name.split("-")[1]),
+        )
+        for checkpoint_path in checkpoint_paths:
+            print("*" * 10)
+            logger.info(f"Evaluating checkpoint: {checkpoint_path}")
 
-        # 候補トークン「1,2,3」のtoken idを取得
-        candidate_ids = [tokenizer.convert_tokens_to_ids(tok) for tok in CANDIDATE]
-        logger.info(f"Candidate token IDs: {candidate_ids}")
+            val_df = df[df["fold"] == fold].reset_index(drop=True)
+            logger.info(f"Number of validation samples: {len(val_df)}")
 
-        scores = generate_text_vllm(cfg, val_df["prompt"], tokenizer, lora_path=cfg.exp.lora_checkpoint)
+            scores = generate_text_vllm(cfg, val_df["prompt"], tokenizer, lora_path=checkpoint_path)
 
-        # 結果をデータフレームに追加
-        val_df["pred_label"] = np.argmax(scores, axis=1)
+            # 結果をデータフレームに追加
+            val_df["pred_label"] = np.argmax(scores, axis=1)
 
-        y_label_zero = val_df["y_label"] - 1
+            y_label_zero = val_df["y_label"]
 
-        # 精度評価
-        accuracy = (val_df["pred_label"] == y_label_zero).mean()
-        logger.info(f"Validation Accuracy: {accuracy:.4f}")
-        cm = confusion_matrix(y_label_zero, val_df["pred_label"])
-        logger.info(f"Confusion Matrix:\n{cm}")
+            # 精度評価
+            accuracy = (val_df["pred_label"] == y_label_zero).mean()
+            logger.info(f"Validation Accuracy: {accuracy:.4f}")
+            cm = confusion_matrix(y_label_zero, val_df["pred_label"])
+            logger.info(f"Confusion Matrix:\n{cm}")
+
+            # easyの確率と閾値ごとのprecision, recall, f1を計算
+            easy_probs = scores[:, 0]
+            thresholds = np.linspace(0, 1, 11)
+            for threshold in thresholds:
+                pred_label = (easy_probs > threshold).astype(int)
+                precision = np.sum((pred_label == 1) & (y_label_zero == 0)) / np.sum(pred_label == 1)
+                recall = np.sum((pred_label == 1) & (y_label_zero == 0)) / np.sum(y_label_zero == 0)
+                f1 = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0
+                logger.info(
+                    f"Threshold: {threshold:.2f}, Num of pred easy: {np.sum(pred_label == 1)}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}"
+                )
 
         # 結果の保存
-        result_file = output_dir / "inference_results.csv"
-        val_df.to_csv(result_file, index=False)
-        logger.info(f"Results saved to {result_file}")
+        # result_file = output_dir / "inference_results.csv"
+        # val_df.to_csv(result_file, index=False)
+        # logger.info(f"Results saved to {result_file}")
 
 
 @hydra.main(version_base=None, config_path=".", config_name="config")

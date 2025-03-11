ad92fc538ec9ca58b2b386cd60264f5181378d05
tak-aki <aki-5@hotmail.co.jp>
2025-03-11 18:04:59 +0000

    difficultyモデルのチェックポイントパスを更新

--- codes_fixer/difficulty.py
+++ codes_fixer/difficulty.py
@@ -68,7 +68,7 @@ def get_easy_probs(problem_statements: list[str], return_model: bool=False) -> n
     if os.getenv("KAGGLE_KERNEL_RUN_TYPE") or os.getenv("KAGGLE_IS_COMPETITION_RERUN"):
         llm_model_pth: str = "/kaggle/input/m/mtfall/deepseek-r1/transformers/deepseek-r1-distill-llama-70b-awq/1"
         difficulty_lora_path: str = (
-            "/kaggle/input/kprize-akami-difficulty-model/output_train-exp003-70b_003-fold0-checkpoint-100"
+            "/kaggle/input/kprize-akami-difficulty-model/output_train-exp004-70b_003-fold0-checkpoint-100"
         )
         num_gpus: int = 4
     else:

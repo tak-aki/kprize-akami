0e50c7de874ac95a20bc75bdb032bd7380b39335
tak-aki <aki-5@hotmail.co.jp>
2025-03-11 07:42:07 +0000

    retrieve関数にeasy_probsを追加し、問題の難易度に基づく予測スキップのロジックを実装。easy_probをデータに含めるように更新。

--- codes_fixer/retrieval_wrapper.py
+++ codes_fixer/retrieval_wrapper.py
@@ -10,6 +10,7 @@ import pandas as pd
 start_time = time.time()
 
 from .config import BATCH_SIZE 
+from .difficulty import get_easy_probs
 from .llm_selection import get_llm_selection
 from .bm25 import get_bm25_top_files
 from .llm_retrieve import get_llm_retrieval
@@ -27,10 +28,17 @@ def retrieve(
     """
     if skip_prediction:
         return None
+    
+    easy_probs = get_easy_probs([problem_statement])
+    easy_prob = easy_probs[0]
+    # if easy_prob < difficulty_threshold:
+    #     logger.info(f"Skipping prediction because the problem is too difficult (easy_prob={easy_prob:.2f})")
+    #     return None
 
     directory_string = stringify_directory(directory)
 
     selection_completion_texts, llm_selected_files = get_llm_selection(directory_string, problem_statement)
+
     bm25_top_files = get_bm25_top_files(problem_statement, directory, top_k=30)
 
     concat_files = [sf + [bf for bf in bm25_top_files if bf not in sf] for sf in llm_selected_files] # llm selectionにあるファイルはbm25から除去しつつ結合
@@ -39,6 +47,7 @@ def retrieve(
 
     data = {
             "problem_statement": [problem_statement for _ in range(BATCH_SIZE)],
+            "easy_prob": [easy_prob for _ in range(BATCH_SIZE)],
             "llm_selection_completion_texts": selection_completion_texts,
             "llm_selected_files": llm_selected_files,
             "bm25_top_files": [bm25_top_files for _ in range(BATCH_SIZE)],
